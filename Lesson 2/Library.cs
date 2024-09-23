47ebd039401efe167eed084e0a9b5d73482137eb
ЕвгенияПокидина <ovee9191@gmail.com>
2024-09-23 14:42:57 +0300

    Lesson 2

---
+++ Lesson 2/Library.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+    }
+    internal class Library
+    {
+        private List<Book> books;
+        public Library()
+        {
+            books = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine($"Книга {book.Title} добавлена в библиотеку");
+        }
+        public void RemoveBook(string title)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Title.ToLower() == title.ToLower())
+                {
+                    books.RemoveAt(i);
+                    Console.WriteLine($"Книга {title} удалена из библиотеки.");
+                    return;
+                }
+            }
+            Console.WriteLine($"Книга {title} не найдена в библиотеке.");
+        }
+        public void SortBooks()
+        {
+            books = books.OrderBy(b => b.Year).ToList();
+        }
+        public void Print()
+        {
+            Console.WriteLine("Книги в библиотеке:");
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.Title);
+              
+            }
+        }
+    }
+}
+
+
+   
+
+    
+
+    
+
+
+
--- Lesson 2/Program.cs
+++ Lesson 2/Program.cs
@@ -1,7 +1,24 @@
 ﻿
 using Lesson_2;
+using System.Threading.Channels;
+//1
+//DynamicArray array = new DynamicArray();
+//array.Print();
+//array.Arrange();
+//array.Print();
 
-DynamicArray array = new DynamicArray();
-array.Print();
-array.Arrange();
-array.Print();
+//2
+Library library = new Library();
+library.AddBook(new Book("Война и мир", "Лев Толстой", 1869));
+library.AddBook(new Book("Собачье сердце", "Михаил Булгаков", 1925));
+library.Print();
+library.SortBooks();
+Console.Write("Книги после сортировки");
+library.Print();
+library.RemoveBook("Война и мир");
+
+
+
+
+
+
