18caaf35943b09c1da7f797e05b7e5e1f42cf80c
Евгения Покидина <ovee91@inbox.ru>
2024-11-10 17:16:17 +0300

    Lesson 13

---
+++ Lesson 13/Animal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_13
+{
+    public abstract class Animal
+    {
+        public int Id { get; set; }
+        public  string? Name { get; set; }
+        public abstract void CalculateFood();
+        public abstract int GetFoodAmount();
+    }
+    public class Carnivore : Animal
+    { 
+        public int DailyMeat { get; set; }
+        public Carnivore(int id, string name, int dailyMeat)
+        {
+            Id = id;
+            Name = name;
+            DailyMeat = dailyMeat;
+        }
+
+        public override void CalculateFood()
+        {
+            Console.WriteLine($"{Name} - хищник. Ему необходимо {DailyMeat} гр мяса в день");
+        }
+        public override int GetFoodAmount()
+        {
+            return DailyMeat;
+        }
+    }
+    public class Omnivore : Animal
+    { 
+        public int DailyFood { get; set; }
+
+        public Omnivore(int id, string name, int dailyFood)
+        {
+            Id = id;
+            Name = name;
+            DailyFood = dailyFood;
+        }
+
+        public override void CalculateFood()
+        {
+            Console.WriteLine($"{Name} - всеядный. Ему необходимо {DailyFood} гр пищи в день");
+        }
+        public override int GetFoodAmount()
+                 {
+   return DailyFood;
+        }
+    }
+    public class Herbivore : Animal 
+    {
+        public int DailyGrass { get; set; }
+
+        public Herbivore(int id, string name, int dailyGrass)
+        {
+            Id = id;
+            Name = name;
+            DailyGrass = dailyGrass;
+        }
+        public override void CalculateFood()
+        {
+            Console.WriteLine($"{Name} - травоядный. Ему необходимо {DailyGrass} гр травы в день");
+        }
+        public override int GetFoodAmount()
+        {
+            return DailyGrass;
+        }
+    }
+}
---
+++ Lesson 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lesson_13;
+string filePath = "animals.txt";
+List<Animal> animals = new List<Animal>
+{
+new Carnivore(1, "Лев", 500),
+new Omnivore(2, "Медведь", 2500),
+new Herbivore(3, "Слон", 15000),
+new Carnivore(4, "Тигр", 700),
+new Omnivore(5, "Волк", 1000),
+new Herbivore(6, "Овца", 3000)
+};
+SaveAnimalsToFile(filePath, animals);
+static void SaveAnimalsToFile(string filePath, List<Animal> animals)
+{
+    using (StreamWriter writer = new StreamWriter(filePath))
+    {
+        foreach (var animal in animals)
+        {
+            writer.WriteLine($"{animal.Id},{animal.Name},{animal.GetType().Name},{animal.GetFoodAmount()}");
+        }
+    }
+    Console.WriteLine("Список животных по количеству пищи:");
+    foreach (var animal in animals)
+    {
+        animal.CalculateFood();
+        Console.WriteLine($"ID: {animal.Id}, Имя: {animal.Name}, Тип: {animal.GetType().Name}");
+    }
+}
