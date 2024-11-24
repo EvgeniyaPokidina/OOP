df8403e8b721191a389626abbdd27399c9db0f0c
Евгения Покидина <ovee91@inbox.ru>
2024-11-24 17:31:48 +0300

    Lesson 26

---
+++ Lesson 26/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_26
+{
+    public class Counter
+    {
+        private int count;
+        public Counter(int Count)
+        {
+            count = Count;
+        }
+        public void Increment()
+        {
+            lock (this)
+            {
+                count++;
+                Console.WriteLine($"Увеличение счетчика: {count}");
+            }
+        }
+        public void Decrement()
+        {
+            lock (this)
+            {
+                count--;
+                Console.WriteLine($"Уменьшение счетчика: {count}");
+            }
+        }
+        public int GetValue()
+        {
+            lock (this)
+            {
+                return count;
+            }
+        }
+    }
+}
---
+++ Lesson 26/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lesson_26;
+
+Counter counter = new Counter(0);
+Thread incrementThread = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        counter.Increment();
+        Thread.Sleep(1000);
+    }
+});
+Thread decrementThread = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        counter.Decrement();
+        Thread.Sleep(1000); 
+    }
+});
+incrementThread.Start();
+decrementThread.Start();
+incrementThread.Join();
+decrementThread.Join();
+Console.WriteLine($"Текущее значение счетчика: {counter.GetValue()}");
+    
