ac82a8dfe7eccca3c3a55c42cf7e73d846dbe0b7
Евгения Покидина <ovee91@inbox.ru>
2024-11-26 22:49:24 +0300

    Lesson 26.2

---
+++ Lesson 26.2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Thread firstThread = new Thread(() => new Person().Print(new Person()));
+firstThread.Start();
+class Person
+{
+
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public void Print(object? message)
+    {
+        if (message is not Person)
+        {
+            throw new Exception("Передан не подходящий объект");
+        }
+        else
+        {
+            PrintPersonData((Person)message);
+        }
+
+    }
+    public void PrintPersonData (Person person)
+    {
+        person.Name = "Виктор";
+        person.Age = 22;
+        Console.WriteLine($"Имя: {person.Name}  Возраст: {person.Age}");
+    }
+
+}
+
+
+
+
+
