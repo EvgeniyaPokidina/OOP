3883b1eacd39a3c7e1b366899afd00d66cddca28
Евгения Покидина <ovee91@inbox.ru>
2024-12-14 23:03:29 +0300

    Lesson 30.2

---
+++ Lesson 30.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using Lesson_30._2;
+using System.Xml.Serialization;
+
+User user=new User()
+{
+    Name = "Ivan",
+    Age = 40
+};
+User user1=new User()
+{
+    Name = "Petr",
+    Age = 22
+};
+User user2=new User()
+{
+    Name = "Sam",
+    Age = 34
+};
+//List<User> users = new List<User>();
+//users.Add(user);
+//users.Add(user1);
+//users.Add(user2);
+//XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<User>));
+//FileStream stream = new FileStream("user.xml", FileMode.Create);
+//xmlSerializer.Serialize(stream, users);
+//stream.Close();
+//using (FileStream stream = new FileStream("user.xml", FileMode.Create))
+//{
+//    xmlSerializer.Serialize(stream, users);
+//}
+
+XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<User>));
+using (FileStream stream = new FileStream("user.xml", FileMode.Open))
+{
+    List<User> users = (List<User>)xmlSerializer.Deserialize(stream)!;
+    foreach (User userr in users)
+    {
+        Console.WriteLine(user);
+    }
+}
---
+++ Lesson 30.2/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lesson_30._2
+{
+    [Serializable]
+    public class User
+    {
+        public string? Name { get; set; }
+        public int Age {  get; set; }
+    }
+    
+}
